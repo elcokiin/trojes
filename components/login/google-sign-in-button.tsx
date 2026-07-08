3c9c055984078247921a0ad4b6f84b6b6c5577e4
Diego Tenjo <tenjodiego961@gmail.com>
2026-07-08 03:05:31 -0500

    chore: commit DRY refactor of Google logo paths and more stars in login background

--- components/login/google-sign-in-button.tsx
+++ components/login/google-sign-in-button.tsx
@@ -4,13 +4,21 @@ import { use3DButton } from "@/hooks/use-3d-button";
 import { useIsMobile } from "@/hooks/use-mobile";
 import { Button } from "@/components/ui/button";
 
-function GoogleIcon() {
+function GoogleLogoPaths() {
   return (
-    <svg viewBox="0 0 24 24" className="size-6">
+    <>
       <path fill="#4285F4" d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" />
       <path fill="#34A853" d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" />
       <path fill="#FBBC05" d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" />
       <path fill="#EA4335" d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" />
+    </>
+  );
+}
+
+function GoogleIcon() {
+  return (
+    <svg viewBox="0 0 24 24" className="size-6">
+      <GoogleLogoPaths />
     </svg>
   );
 }
@@ -351,23 +359,8 @@ export function GoogleSignInButton({ onClick }: { onClick?: () => void }) {
                 stroke="var(--wood-inner-stroke)"
                 strokeWidth={1.4}
               />
-              <g transform="translate(40,25)">
-                <path
-                  fill="#EA4335"
-                  d="M13.5 11.2v4.6h6.4c-.28 1.5-1.13 2.77-2.4 3.62v3h3.87c2.27-2.1 3.58-5.2 3.58-8.88 0-.86-.08-1.68-.22-2.47H13.5z"
-                />
-                <path
-                  fill="#34A853"
-                  d="M13.5 26c3.24 0 5.96-1.07 7.95-2.9l-3.87-3c-1.07.72-2.45 1.15-4.08 1.15-3.14 0-5.8-2.12-6.75-4.97H2.75v3.1C4.73 23.3 8.83 26 13.5 26z"
-                />
-                <path
-                  fill="#FBBC05"
-                  d="M6.75 16.28c-.24-.72-.38-1.49-.38-2.28s.14-1.56.38-2.28v-3.1H2.75A12.9 12.9 0 0 0 1.4 14c0 1.98.48 3.86 1.35 5.38l4-3.1z"
-                />
-                <path
-                  fill="#4285F4"
-                  d="M13.5 6.75c1.77 0 3.35.6 4.6 1.8l3.42-3.42C19.45 3.2 16.74 2 13.5 2c-4.67 0-8.77 2.7-10.75 6.62l4 3.1c.95-2.85 3.61-4.97 6.75-4.97z"
-                />
+              <g transform="translate(42,27)">
+                <GoogleLogoPaths />
               </g>
               <text
                 x={200}
